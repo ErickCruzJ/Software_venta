96b9e1d70bfdc824841b457dd6cb853236bdc21c
Erick Cruz <erick.cruzdj@gmail.com>
2026-07-17 14:27:10 -0600

    Modelos

--- app/Models/precio.php
+++ app/Models/precio.php
@@ -4,7 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class precio extends Model
+class Precio extends Model
 {
-    //
+    protected $primaryKey = 'id_precio';
+
+    protected $fillable = [
+        'id_producto',
+        'precio_venta',
+        'fecha',
+        'estado',
+    ]
+    
+    publuc function casts():array
+    {
+        return[
+            'fecha' => 'date',
+            'estado' => 'boolean',
+        ];
+    }
+
+    public function producto
+    {
+        return $this->belonTo(Producto::Class, 'id_producto');
+    }
 }
